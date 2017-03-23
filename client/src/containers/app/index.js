640ec079604a31aef2b53d60517e95ac99fa6f04
Nick Lee <nicklee808@gmail.com>
2017-03-22 19:34:35 -1000

    components, containers

--- client/src/containers/app/index.js
+++ client/src/containers/app/index.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import './styles.css';
+import { bookList } from './data.js';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+class App extends Component {
+  constructor(){
+    super();
+    this.title = 'Kanban';
+    this.bookList = { bookList };
+  }
+
+  doClick = () => {
+    console.log( this.title );
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <BookListAppTitle
+          title={this.title}
+          doClick={this.doClick}
+        />
+        <BookListAppSearch/>
+        <BookListAppList
+          list={ bookList }
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
