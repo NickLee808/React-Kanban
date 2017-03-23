4019d2f6e684efe4302112a667e22ffafb1fb5b1
Nick Lee <nicklee808@gmail.com>
2017-03-22 14:13:52 -1000

    card.js

--- routes/card.js
+++ routes/card.js
@@ -1,3 +1,35 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../models');
+const { Card } = db;
+
+
+router.route('/')
+.get((req, res) => {
+  Card.findAll()
+  .then(cards => {
+    res.json({cardList:{cards}});
+  });
+})
+.post((req, res) => {
+  Card.create({
+    title: req.body.title,
+    priority: req.body.priority,
+    status: req.body.status
+  })
+  .then((task) => {
+    res.send('POSTED');
+  });
+});
+
+router.route('/:id')
+.delete((req, res) => {
+  Card.destroy(
+  {
+    where: {
+      id: req.params.id
+    }
+  });
+});
+
+module.exports = router;
