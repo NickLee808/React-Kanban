95475833263d8b5989de0881f8b95ddb44ca9024
Nick Lee <nicklee808@gmail.com>
2017-04-05 16:42:10 -1000

    added post form in header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,7 +4,16 @@ const Header = (props) => (
   <div className='App-header'>
     <h2>{ props.title }</h2>
     <form action="/action_page.php">
-      <select name="cars">
+      Name:
+      <input type="text" name ="name"></input>
+      Priority:
+      <select name="priority">
+        <option value="low">Low</option>
+        <option value="low">Medium</option>
+        <option value="low">High</option>
+      </select>
+      Status:
+      <select name="status">
         <option value="queue">In queue</option>
         <option value="progress">In progress</option>
         <option value="done">Done</option>
